d7ce9e7e89e2a3400ab9cd4388ea3c6954d42352
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-11-02 09:48:42 -0500

    feat: add doc interface that matches what's expected to be retrieved from the backend

---
+++ src/app/interfaces/doc.ts
@@ -0,0 +1,14 @@
+export interface Doc {
+  name: string;
+  userType: string;
+  email: string;
+  password: string;
+  dni: string;
+  age: number;
+  speciality: string;
+  experienceYears: number;
+  description: string;
+  patientsAssisted: number;
+  doctorFee: number;
+  profilePhoto: string;
+}
